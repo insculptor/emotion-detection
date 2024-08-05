69602e24aa5b8074937f37e6a3491d061a493eb9
Ravi Dhir <er.dr.rd@gmail.com>
2024-08-05 16:13:51 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOPS End-to-End Pipeline showing Emotion Detection on sample data.',
+    author='ravi_dhir',
+    license='MIT',
+)
